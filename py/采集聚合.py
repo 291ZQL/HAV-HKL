087abfd612200036e0437e16c50c4ffa4574a852
FGBLH <pengli08211@gmail.com>
2026-07-25 21:55:37 +0800

    Update 采集聚合.py

--- py/采集聚合.py
+++ py/采集聚合.py
@@ -46,6 +46,7 @@ class Spider(Spider):
         's33': {'name': '🐾魔爪', 'api': 'https://mozhuazy.com/api.php/provide/vod'},
         's34': {'name': '📺魔都', 'api': 'https://www.mdzyapi.com/api.php/provide/vod'},
         's35': {'name': '📺西瓜资源', 'api': 'https://caiji.xgzyapi.com/api.php/provide/vod/'},
+        's36': {'name': '📺98资源', 'api': 'https://98zy.vip/api.php/provide/vod/'},
     }
 
     headers = {
