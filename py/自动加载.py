acaa56de43758846899199fc3f55e5a686af8e07
FGBLH <pengli08211@gmail.com>
2026-07-28 00:45:30 +0800

    Update 自动加载.py

--- py/自动加载.py
+++ py/自动加载.py
@@ -17,7 +17,7 @@ class Spider(Spider):
     PY_DIR    = "/storage/emulated/0/海豚影视/海豚/py"
     JS_DIR    = "/storage/emulated/0/海豚影视/海豚/js"
     JAR_DIR   = "/storage/emulated/0/海豚影视/海豚/jar"
-    SAVE_PATH = "/storage/emulated/0/海豚影视/海豚/自动接口.json"
+    SAVE_PATH = "/storage/emulated/0/海豚影视/海豚/海豚影视.json"
     LOGO_PATH = "/https://img.freepik.com/free-vector/cute-dolphin-swimming-cartoon-vector-icon-illustration-animal-nature-icon-isolated-flat-vector_138676-12582.jpg?semt=ais_hybrid&w=740&q=80"
 
     # 🔒 锁定在 sites 第 0、1 位的配置，无论扫描结果如何始终存在
