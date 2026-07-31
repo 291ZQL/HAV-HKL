e3886bb34cf7e9890c86a9e013e39d7e48840505
FGBLH <pengli08211@gmail.com>
2026-08-01 01:04:45 +0800

    Update 自动加载.py

--- py/自动加载.py
+++ py/自动加载.py
@@ -23,6 +23,12 @@ class Spider(Spider):
     # 🔒 锁定在 sites 第 0、1 位的配置，无论扫描结果如何始终存在
     _LOCKED_SITES = [
 		{
+            "name": "🐬自动加载 海豚影视交流群 TG：@hshsjk9",
+            "key": "自动加载",
+            "type": 3,
+            "api": "https://ghfast.top/https://raw.githubusercontent.com/FGBLH/HKL/refs/heads/main/py/自动加载.py"
+         },
+		{
             "name": "🐬弹幕 海豚影视交流群 TG：@hshsjk9",
             "key": "弹幕豆瓣",
             "type": 3,
